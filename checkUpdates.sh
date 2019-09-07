bef89c8c3b729930f3586be519787ff75b4cc39d
spaceface777 <spaceface7777@gmail.com>
2019-09-07 14:45:15 +0200

    Many improvements and fixes

--- checkUpdates.sh
+++ checkUpdates.sh
@@ -1,29 +1,54 @@
 #!/bin/bash
 
-Domain="hermesmessenger.duckdns.org"
-Token=$1
+Date=`date +"%Y-%m-%d %T"`
+Version="1.1.0"
+
+echo "[DEBUG] Running HADaemon v$Version at $Date"
+
+Token="INSERT_DNS_TOKEN_HERE"
+Domain="hermesmessenger.chat"
+
+Hosts=("testing" "@" "www")
+MyHosts=("server1" "db1")
+
+if !(( ${#Hosts} && ${#MyHosts} )); then
+    echo -e "\n[ERROR] The DNS hosts are unset, please edit this script and set them."
+    exit 1
+fi
 
 DomainIP=$(host $Domain | awk '/has address/ { print $4 ; exit }')
+MyIP=$(host ${MyHosts[0]}.$Domain | awk '/has address/ { print $4 ; exit }')
 
-ExternalIP=$(wget -q -O - "http://myexternalip.com/raw")
+if [ $DomainIP == $MyIP ] ; then
 
-if [ $DomainIP = $ExternalIP ] ; then
-
-    echo "This IP is running the server"
+    echo -e "\n[DEBUG] This server is currently active, no need to do anything"
 
 else
-    echo "This IP is not running the server, checking if server is up..."
-    
-    ping -c1 -W1 -q $DomainIP &>/dev/null
+    echo -e "\n[DEBUG] This server is not currently active, checking if active server is up..."
+
+    curl -sL -w "%{http_code}\n" $Domain -o /dev/null &> /dev/null
 
     if [ $? == 0 ] ; then
-        echo "Server is up, no need to do anything. "
+        echo -e "\n[DEBUG] Active server is up, no need to do anything. " # TODO: Check active server's priority
 
     else
-        echo "Server isn't up, setting domain IP address to this server"
+        echo -e "\n[DEBUG] Active server is down, updating DNS entries to this server"
 
-    	echo url="https://www.duckdns.org/update?domains=$Domain&token=$Token&verbose=true" | curl -k -o ~/Scripts/DuckDNS.log -K - 
+        len=${#Hosts[@]}
+        for (( i=0; i<${len}; i++ )); do
+            echo url="https://dynamicdns.park-your-domain.com/update?host=$Host&domain=${Hosts[$i]}&password=$Token" | curl -K - &> /dev/null
+            echo "        -  Updated domain $((i + 1))/$len: ${Hosts[$i]}.$Domain"
+        done
 
     fi
-
 fi
+
+echo -e "\n[DEBUG] Updating this server's DNS in case it's changed..."
+
+len=${#MyHosts[@]}
+for (( i=0; i<${len}; i++ )); do
+    echo url="https://dynamicdns.park-your-domain.com/update?host=$MyDomain&domain=${MyHosts[$i]}&password=$Token" | curl -K - &> /dev/null
+    echo "        -  Updated domain $((i + 1))/$len: ${MyHosts[$i]}.$Domain"
+done
+
+echo -e "\n[DEBUG] All done."
